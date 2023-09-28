cb8871676f73a439f66ff8bf8df45566c4f2af3a
Victor <victor.kyalo@student.moringaschool.com>
2023-09-28 08:44:45 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+// Define the addingEventListener function
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    // Add a click event listener to the input element
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  }
+  
+ 
